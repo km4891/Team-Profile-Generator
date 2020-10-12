ac5b359adc2eda2df20691f0db2ec24d55b348b4
km4891 <kmorarity@gmail.com>
2020-10-12 00:54:37 -0700

    fixed looping

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 const inquirer = require('inquirer');
+const generatePage = require ('./src/page-template');
+const { writeFile, copyFile } = require('./util/generate-site')
 
 const managerInput = () => {
     return inquirer.prompt([ {
@@ -36,19 +38,17 @@ const managerInput = () => {
 
     ])
 
-    .then(response => {
-        const newManager = new Manager(response.name, response.id, response.email, response.phoneNumber);
-        employees.push(newManager);
-        staffInput();
-    });
+      
 };
 
-    const engineerInput = () => {
+
+
+    const staffInput = (team) => {
         return inquirer.prompt([ {
         
             type: 'input',
             name: 'name',
-            message: "Enter an engineer's name. (Required)",
+            message: "Enter an employee's name. (Required)",
             validate: name => {
                 if (name) {
                     return true;
@@ -61,57 +61,82 @@ const managerInput = () => {
         {
             type: 'input',
             name: 'id',
-            message: "Enter an engineer's ID.",
+            message: "Enter an empployee's ID.",
         },
+        
         {
             type: 'input',
             name: 'email',
-            message: "Enter an engineer's email.",
+            message: "Enter an employees's email.",
+        },
+        {
+            type: 'list',
+            name: 'role',
+            message: "Enter employee's role.",
+            choices: ['Engineer', 'Intern'],
         },
         {
             type: 'input',
             name: 'github',
-            message: "Enter an engineer's Github username."
-        }
-    
-        ])
-    };
-
-    const internInput = () => {
-        return inquirer.prompt([ {
-        
-            type: 'input',
-            name: 'name',
-            message: "Enter an intern's name. (Required)",
-            validate: name => {
-                if (name) {
-                    return true;
-                } else {
-                    console.log('Please enter a valid response!');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'id',
-            message: "Enter an intern's ID.",
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: "Enter an intern's email.",
+            message: "Enter an engineer's Github username.",
+            when: (member) => {
+                return member.role === 'Engineer'
+            },
         },
         {
             type: 'input',
             name: 'school',
-            message: "Enter the school name the intern attends."
-        }
-    
+            message: "Enter the school name the intern's attends.",
+            when: (member) => {
+                return member.role === 'Intern'
+            },
+        },
+        {
+            type: 'confirm',
+            name: 'addEmployee',
+            message: "Add another employee?",
+            default: false,
+        },
         ])
-    };
+        .then (employee => {
+            if (employee.role === 'Engineer') {
+                const {name, id, email, github} = employee;
+                team.staff.push(new Engineer(name, id, email, github))
+            } else if (employee.role === 'Intern') {
+                const {name, id, email, school} = employee;
+                team.staff.push(new Intern(name, id, email, school))
+            } 
+            if (employee.addEmployee) {
+                return staffInput(team)
+            } else {
+                return team;
+            }
+          })
+       };
 
-    internInput();
+       managerInput()
+        .then (managerResponse => {
+            const {name, id, email, phoneNumber} = managerResponse;
+            const manager = new Manager(name, id, email, phoneNumber);
+
+            return {
+                manager,
+                staff: [],
+            }
+        })
+        .then (staffInput)
+        .then(generatePage)
+        .then ( (data) => writeFile(data))
+        .then(writeFileResponse => {
+            console.log(writeFileResponse);
+            return copyFile();
+       })
+        .then(copyFileResponse => {
+            console.log(copyFileResponse);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+       
     
 
-    
